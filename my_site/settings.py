0ab3d2a6359227d2abd2df5719ad612bba7db502
Mohamed Sarhan <70814337+MohamedSarhan7@users.noreply.github.com>
2022-12-31 20:13:38 +0200

    Add files via upload

--- my_site/settings.py
+++ my_site/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import django_heroku
-from msilib.schema import Media
+# import django_heroku
+import django_on_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,4 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT=BASE_DIR / "uploads"
 MEDIA_URL = "/image/"
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
+django_on_heroku.settings(locals(),staticfiles=False)
